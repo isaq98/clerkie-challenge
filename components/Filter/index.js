dee9e202e3b0b4d4c67ca50599f45b3be7de3574
Sameer Isaq <shisaq@dons.usfca.edu>
2023-05-22 12:37:39 -0700

    Filter functionality works but need to fix UI

--- components/Filter/index.js
+++ components/Filter/index.js
@@ -1,27 +1,53 @@
 import './_Filter.css';
 
 function Filter(props) {
+
+    const { setFiltersCallback, setApplyCallback } = props;
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        setApplyCallback(true);
+    }
+
+    const handleChange = (event) => {
+        const {value, checked} = event.target;
+        if(checked) {
+            setFiltersCallback((currValues) => [...currValues, value]);
+        }
+        else {
+            setFiltersCallback((currValues) => currValues.filter((element) => element !== value));
+        }
+    }
+
     return (
         <div className="filter-container">
             <div className="filter-header">
-                <button className="clear-filters-button">Clear all</button>
-                <h4>Filter</h4>
-                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L13 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/><path d="M13 4L4 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/></svg>
-            </div>
-            <div className="filter-options">
-                <h5>Friend Status</h5>
                 <div>
-                        <label>Close Friends</label>
-                        <input type="checkbox" value="Close Friends"></input><br></br>
-                    
-                        <label>Super Close Friends</label>
-                        <input type="checkbox" value="Super Close Friends"></input>
-                    
+                <button className="clear-filters-button">Clear all</button>
+                </div>
+                <div>
+                <h4>Filter</h4>
+                </div>
+                <div>
+                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L13 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/><path d="M13 4L4 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/></svg>
                 </div>
             </div>
-            <div className="filter-footer">
-                <button className="apply-filters-button">Apply</button>
-            </div>
+            <form onSubmit={onSubmitHandler}>
+                <div className="filter-options">
+                    <p>Friend Status</p>
+                    <div className="checkbox-container">
+                        <label htmlFor="Close Friends">Close Friends</label>
+                        <input type="checkbox" name="status" id="Close Friends" value="Close Friends" onChange={handleChange}/>
+                    </div>
+                    <div className="checkbox-container">
+                        <label htmlFor="Super Close Friends">Super Close Friends</label>
+                        <input type="checkbox" name="status" id="Super Close Friends" value="Super Close Friends" onChange={handleChange}/>
+                    </div>
+                </div>
+                <div className="filter-footer">
+                    <button type="submit" className="apply-filters-button">Apply</button>
+                </div>
+            </form>
         </div>
     )
 }
--- components/Friends/index.js
+++ components/Friends/index.js
@@ -1,24 +1,30 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { mockFriendsObject } from "@/constants/enums";
 import Filter from '../Filter';
 import "./_Friends.css";
 
-function FilterButton() {
+/* TODO: Feel like this component can be cleaned up too */
+
+function FilterButton(props) {
+    const { setActiveFilters, setApplyFilters } = props;
     const [filterVisibility, setFilterVisibility] = useState(false);
     return (
         <div className="filter-content">
-            <button className="filter-button" onClick={() => setFilterVisibility(!filterVisibility)}>
+            <button className={`filter-button${filterVisibility ? ' active' : ''}`} onClick={() => setFilterVisibility(!filterVisibility)}>
                 <svg width="19" height="21" viewBox="0 0 19 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 3.5H18" stroke="#424242" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><circle cx="3" cy="3.5" r="2.25" fill="white" stroke="#424242" strokeWidth="1.5"/><path d="M17 10.5H1" stroke="#424242" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><circle cx="3" cy="3" r="2.25" transform="matrix(-1 0 0 1 19 7.5)" fill="white" stroke="#424242" strokeWidth="1.5"/><path d="M2 17.5H18" stroke="#424242" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><circle cx="3" cy="17.5" r="2.25" fill="white" stroke="#424242" strokeWidth="1.5"/></svg>
             </button>
             <svg className="svg-divider" width="2" height="30" viewBox="0 0 2 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L1 29" stroke="#D7D7D7" strokeLinecap="round" strokeLinejoin="round"/></svg>
             <button className="clear-filters-button">Clear all</button>
-            {filterVisibility && <Filter />}
+            {filterVisibility && <Filter setFiltersCallback={setActiveFilters} setApplyCallback={setApplyFilters} />}
         </div>
     )
 }
 
 function Friends() {
+    const [activeFilters, setActiveFilters] = useState([]);
+    const [filteredFriends, setFilteredFriends] = useState(mockFriendsObject);
+    const [applyFilters, setApplyFilters] = useState(false);
 
     const determineFriendStatus = (status) => {
         switch(status) {
@@ -32,7 +38,25 @@ function Friends() {
     }
 
     const renderFriends = () => {
-        return mockFriendsObject.map((friendElem, idx) => {
+        if(activeFilters.length === 0) {
+            setFilteredFriends(mockFriendsObject);
+        }
+        else {
+            let filteredItems = mockFriendsObject.filter((element) => activeFilters.includes(element?.friend_state));
+            setFilteredFriends(filteredItems);
+        }
+    }
+    
+    useEffect(() => {
+        renderFriends();
+        setApplyFilters(false);
+    }, [applyFilters])
+
+    return (
+        <div className="friends-list-container">
+            <FilterButton setActiveFilters={setActiveFilters} setApplyFilters={setApplyFilters} />
+            <div className="friends-list">
+                { filteredFriends.map((friendElem, idx) => {
         return ( 
             <div className="friend-card" key={`${friendElem}-${idx}`}>
                 <h4>{friendElem.name} {friendElem?.friend_state ? <span className={`friend-status ${determineFriendStatus(friendElem?.friend_state)}`}>{friendElem?.friend_state}</span> : null}</h4>
@@ -42,14 +66,7 @@ function Friends() {
                     {friendElem.phone}
                 </p>
             </div>
-        )})
-    }
-
-    return (
-        <div className="friends-list-container">
-            <FilterButton />
-            <div className="friends-list">
-                {renderFriends()}
+        )})}
             </div>
         </div>
     )
