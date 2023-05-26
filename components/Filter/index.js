18c712963973a96d9dd9019293f7911d2695d6d8
Sameer Isaq <shisaq@dons.usfca.edu>
2023-05-25 21:14:37 -0700

    Adding some aria-labels

--- components/Filter/index.js
+++ components/Filter/index.js
@@ -56,7 +56,7 @@ function Filter(props) {
                     <p>Filter</p>
                 </div>
                 <div className="exit-filter" onClick={() => setVisibilityCallback(false)}>
-                    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L13 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/><path d="M13 4L4 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/></svg>
+                    <svg aria-label="exit" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 4L13 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/><path d="M13 4L4 13" stroke="#424242" strokeWidth="2" strokeLinecap="round"/></svg>
                 </div>
             </div>
             <form onSubmit={onSubmitHandler}>
--- constants/enums.js
+++ constants/enums.js
@@ -1,11 +1,11 @@
 export const mockNavigationObject = [
     {
         title: 'Home',
-        svg: <svg className="navigation-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="4.43984" y="4.43984" width="15.12" height="15.12" rx="1.32" stroke="#EAEAEA" strokeWidth="1.2"/> <path d="M5 10.75L19 10.75" stroke="#EAEAEA" strokeWidth="1.2" strokeLinecap="round"/> <path d="M9.75 11L9.75 19" stroke="#EAEAEA" strokeWidth="1.2" strokeLinecap="round"/></svg>
+        svg: <svg aria-label="home-logo" className="navigation-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="4.43984" y="4.43984" width="15.12" height="15.12" rx="1.32" stroke="#EAEAEA" strokeWidth="1.2"/> <path d="M5 10.75L19 10.75" stroke="#EAEAEA" strokeWidth="1.2" strokeLinecap="round"/> <path d="M9.75 11L9.75 19" stroke="#EAEAEA" strokeWidth="1.2" strokeLinecap="round"/></svg>
     },
     {
         title: 'Friends',
-        svg: <svg className="navigation-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16.7171" cy="9.82941" r="1.61164" stroke="#EAEAEA" strokeWidth="0.75"/><path d="M19.7632 15.328C19.7632 13.5725 18.3401 12.1494 16.5846 12.1494C15.8692 12.1494 15.2089 12.3858 14.6777 12.7847" stroke="#EAEAEA" strokeWidth="0.85" strokeLinecap="round"/><circle cx="10.3575" cy="8.92143" r="3.13139" stroke="#EAEAEA"/><path d="M15.8048 17.9998C15.8048 14.9915 13.3661 12.5527 10.3577 12.5527C7.34939 12.5527 4.91064 14.9915 4.91064 17.9998" stroke="#EAEAEA" strokeLinecap="round"/></svg>            
+        svg: <svg aria-label="friends-logo" className="navigation-svg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16.7171" cy="9.82941" r="1.61164" stroke="#EAEAEA" strokeWidth="0.75"/><path d="M19.7632 15.328C19.7632 13.5725 18.3401 12.1494 16.5846 12.1494C15.8692 12.1494 15.2089 12.3858 14.6777 12.7847" stroke="#EAEAEA" strokeWidth="0.85" strokeLinecap="round"/><circle cx="10.3575" cy="8.92143" r="3.13139" stroke="#EAEAEA"/><path d="M15.8048 17.9998C15.8048 14.9915 13.3661 12.5527 10.3577 12.5527C7.34939 12.5527 4.91064 14.9915 4.91064 17.9998" stroke="#EAEAEA" strokeLinecap="round"/></svg>            
     }
 ];
 
